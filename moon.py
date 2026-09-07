27fa74964d429a9502e142b8961845ff54130427
marcusmaavoco <marcus.maasalu@voco.ee>
2026-09-07 11:52:05 +0000

    test 1.1 for basic function

---
+++ moon.py
@@ -0,0 +1,12 @@
+"""
+Variable addtion test usin print function.
+"""
+
+word = "spider"
+thousand = "1000"
+ten = 10
+first_half = "full"
+second_half = "moon"
+complete = f"{first_half} {second_half}"
+
+print(complete)
